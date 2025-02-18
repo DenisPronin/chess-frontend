df2a628b1d07855b62f1b779b37e453b4b5c3939
DenisPronin <d.pronin@velvetech.dev>
2025-02-18 22:58:29 +0100

    Add global styles

--- src/features/Auth/components/AuthLogin/AuthLogin.tsx
+++ src/features/Auth/components/AuthLogin/AuthLogin.tsx
@@ -3,41 +3,43 @@ import { Button, Card, Flex, Heading, TextField } from '@radix-ui/themes'
 
 export function AuthLogin() {
   return (
-    <Flex minHeight="100vh" align="center" justify="center" className="w-full">
-      <Card className="max-w-md">
-        <Heading as="h2" weight="bold" size="4">
-          Login
-        </Heading>
+    <Flex minHeight="100dvh" align="center" justify="center" p="2">
+      <Card asChild>
+        <Flex direction="column" gap="3" width="400px">
+          <Heading as="h2" weight="bold" size="4">
+            Login
+          </Heading>
 
-        <Form.Root>
-          <Flex direction="column" gap="3" width={{ xs: '100%', md: '360px' }}>
-            <Form.Field name="email">
-              <Form.Label>Email / Username</Form.Label>
-              <Form.Control asChild>
-                <TextField.Root
-                  type="email"
-                  required
-                  placeholder="Enter your email"
-                />
-              </Form.Control>
-            </Form.Field>
+          <Form.Root>
+            <Flex direction="column" gap="3">
+              <Form.Field name="email">
+                <Form.Label>Email / Username</Form.Label>
+                <Form.Control asChild>
+                  <TextField.Root
+                    type="email"
+                    required
+                    placeholder="Enter your email"
+                  />
+                </Form.Control>
+              </Form.Field>
 
-            <Form.Field name="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control asChild>
-                <TextField.Root
-                  type="password"
-                  required
-                  placeholder="Enter your password"
-                />
-              </Form.Control>
-            </Form.Field>
+              <Form.Field name="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control asChild>
+                  <TextField.Root
+                    type="password"
+                    required
+                    placeholder="Enter your password"
+                  />
+                </Form.Control>
+              </Form.Field>
 
-            <Form.Submit asChild>
-              <Button>Sign In</Button>
-            </Form.Submit>
-          </Flex>
-        </Form.Root>
+              <Form.Submit asChild>
+                <Button>Sign In</Button>
+              </Form.Submit>
+            </Flex>
+          </Form.Root>
+        </Flex>
       </Card>
     </Flex>
   )
--- src/features/Theme/Theme.provider.tsx
+++ src/features/Theme/Theme.provider.tsx
@@ -1,5 +1,6 @@
 import { Theme } from '@radix-ui/themes'
 import { ReactNode } from 'react'
+import '@/styles/main.css'
 import '@radix-ui/themes/styles.css'
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
