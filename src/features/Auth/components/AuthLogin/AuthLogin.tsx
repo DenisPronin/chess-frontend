7bdf0eb232cf52b20780684a5123ab9ad626d95c
DenisPronin <d.pronin@velvetech.dev>
2025-02-18 22:24:19 +0100

    Login form markup

--- src/features/Auth/components/AuthLogin/AuthLogin.tsx
+++ src/features/Auth/components/AuthLogin/AuthLogin.tsx
@@ -1,3 +1,44 @@
+import * as Form from '@radix-ui/react-form'
+import { Button, Card, Flex, Heading, TextField } from '@radix-ui/themes'
+
 export function AuthLogin() {
-  return <div>login</div>
+  return (
+    <Flex minHeight="100vh" align="center" justify="center" className="w-full">
+      <Card className="max-w-md">
+        <Heading as="h2" weight="bold" size="4">
+          Login
+        </Heading>
+
+        <Form.Root>
+          <Flex direction="column" gap="3" width={{ xs: '100%', md: '360px' }}>
+            <Form.Field name="email">
+              <Form.Label>Email / Username</Form.Label>
+              <Form.Control asChild>
+                <TextField.Root
+                  type="email"
+                  required
+                  placeholder="Enter your email"
+                />
+              </Form.Control>
+            </Form.Field>
+
+            <Form.Field name="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control asChild>
+                <TextField.Root
+                  type="password"
+                  required
+                  placeholder="Enter your password"
+                />
+              </Form.Control>
+            </Form.Field>
+
+            <Form.Submit asChild>
+              <Button>Sign In</Button>
+            </Form.Submit>
+          </Flex>
+        </Form.Root>
+      </Card>
+    </Flex>
+  )
 }
--- src/features/Game/components/GamePage/GamePage.tsx
+++ src/features/Game/components/GamePage/GamePage.tsx
@@ -1,13 +1,3 @@
-import { Card, Flex, Heading } from '@radix-ui/themes'
-
 export function GamePage() {
-  return (
-    <Flex minHeight="100vh" align="center" justify="center" className="w-full">
-      <Card className="w-full max-w-md">
-        <Heading as="h2" weight="bold" size="4">
-          Login
-        </Heading>
-      </Card>
-    </Flex>
-  )
+  return <div>game</div>
 }
