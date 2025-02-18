916a352a7271ac577f2129d597c7c580c9fd7211
DenisPronin <d.pronin@velvetech.dev>
2025-02-18 22:36:40 +0100

    Update deps, fix eslint

--- src/types/Route.types.ts
+++ src/types/Route.types.ts
@@ -2,6 +2,7 @@ import { JSXElementConstructor } from 'react'
 
 export interface IRoute {
   path: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   element: JSXElementConstructor<any>
   private: boolean
 }
