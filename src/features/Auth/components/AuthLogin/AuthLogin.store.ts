607ff3e165d8d3f2936ff97d7b5cc052f6e5ee1f
DenisPronin <d.pronin@velvetech.dev>
2025-02-21 23:36:14 +0100

    Add logger

--- src/features/Auth/components/AuthLogin/AuthLogin.store.ts
+++ src/features/Auth/components/AuthLogin/AuthLogin.store.ts
@@ -1,6 +1,5 @@
 import { authLogin } from '@/features/Auth/Auth.api'
 import { setAuthToken } from '@/features/Auth/store/Auth.store'
-import { Nullish } from '@/types'
 import { createEffect, createEvent, createStore, sample } from 'effector'
 import { AuthRequestLogin } from '../../Auth.types'
 
@@ -16,13 +15,6 @@ $loginForm.on(changedUsername, (state, username) => ({ ...state, username }))
 $loginForm.on(changedPassword, (state, password) => ({ ...state, password }))
 
 export const loginFx = createEffect(authLogin)
-
-export const $loginStatus = createStore(null)
-export const $loginError = createStore<Nullish<Error>>(null)
-
-$loginStatus.on(loginFx.done, (_, request) => request.result)
-
-$loginError.on(loginFx.fail, (_, request) => request.error)
 
 sample({
   clock: loginFx.done,
