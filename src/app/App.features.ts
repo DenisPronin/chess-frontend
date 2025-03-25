9ae333499041d08db7c6314155a120b44fbb94dc
DenisPronin <d.pronin@velvetech.dev>
2025-03-25 21:32:50 +0100

    Profile feature

--- src/app/App.features.ts
+++ src/app/App.features.ts
@@ -1,7 +1,8 @@
 import { AuthFeature } from '@/features/Auth'
 import { GameFeature } from '@/features/Game'
 import { InitFeature } from '@/features/Init'
+import { ProfileFeature } from '@/features/Profile'
 import { ThemeFeature } from '@/features/Theme'
 import { IFeature } from '@/types'
 
-export const appFeatures: IFeature[] = [ThemeFeature, InitFeature, AuthFeature, GameFeature]
+export const appFeatures: IFeature[] = [ThemeFeature, InitFeature, AuthFeature, GameFeature, ProfileFeature]
---
+++ src/features/Profile/Profile.feature.ts
@@ -0,0 +1,6 @@
+import { IFeature } from '@/types'
+import { FEATURE_NAME } from './Profile.model'
+
+export const ProfileFeature: IFeature = {
+  title: FEATURE_NAME,
+}
